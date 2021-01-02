47599ac3038a1f9875074f4cc19abd29bc4916e8
Austen Bruhn <asbru17@gmail.com>
2021-01-02 22:41:37 +0000

    added PrintBoard() func

--- grid/main.cpp
+++ grid/main.cpp
@@ -3,11 +3,22 @@
 using std::cout;
 using std::vector;
 
+
+// TODO: Add PrintBoard function here.
+void PrintBoard(const vector<vector<int>> &b) {
+  for (const vector<int> &v: b) {
+    for (const int &i: v) {
+      cout << i;
+    }
+    cout << "\n";
+  }
+}
+
 int main() {
     vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
                               {0, 1, 0, 0, 0, 0},
                               {0, 1, 0, 0, 0, 0},
                               {0, 1, 0, 0, 0, 0},
                               {0, 0, 0, 0, 1, 0}};
-
+    PrintBoard(board);
 }
