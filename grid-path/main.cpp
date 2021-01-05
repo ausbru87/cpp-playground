47bfdd6068d9392081847896c9c33ed8a2101c3d
Austen Bruhn <asbru17@gmail.com>
2021-01-05 04:39:20 +0000

    clean up comments

--- grid-path/main.cpp
+++ grid-path/main.cpp
@@ -9,7 +9,6 @@ using std::vector;
 // enumerate cell states
 enum class State {kEmpty, kObstacle, kClosed};
 
-// TODO: Add the ParseLine function here.
 vector<State> ParseLine(std::string line) {
     std::istringstream line_stream(line);
     vector<State> row;
@@ -25,7 +24,6 @@ vector<State> ParseLine(std::string line) {
     return row;
 }
 
-// TODO: Add the ReadBoardFile function here.
 vector<vector<State>> ReadBoardFile(std::string path) {
   std::ifstream board_file(path);
   // add empty board vector
@@ -41,6 +39,9 @@ vector<vector<State>> ReadBoardFile(std::string path) {
   return board;
 }
 
+/** 
+ * Add a node to the open list and mark it as open. 
+ */
 void AddToOpen(int x, int y, int g, int h, vector<vector<int>> &openlist, vector<vector<State>> &grid) {
   openlist.push_back(vector<int>{x, y, g, h});
   grid[x][y] = State::kClosed;
@@ -50,29 +51,44 @@ int Heuristic(int x1, int y1, int x2, int y2) {
   return std::abs(x1 - x2) + std::abs(y1 - y2);
 }
 
+/**
+ * Compare the F values of two cells.
+ */
 bool Compare(vector<int> node1, vector<int> node2) {
   int f1 = node1[2] + node1[3];
   int f2 = node2[2] + node2[3];
   return f1 > f2;
 }
 
-// TODO: Write the Search function stub here.
+/**
+ * Sort the two-dimensional vector of ints in descending order.
+ */
+void CellSort(vector<vector<int>> *v) {
+  sort(v->begin(), v->end(), Compare);
+}
+
+/** 
+ * Implementation of A* search algorithm
+ */
 vector<vector<State>> Search(vector<vector<State>> grid, int init[2], int goal[2]) {
   // Create the vector of open nodes.
   vector<vector<int>> open{};
   
-  // TODO: Initialize the starting node.
+  // Initialize the starting node.
   int x = init[0];
   int y = init[1];
   int g = 0;
   int h = Heuristic(x, y, goal[0], goal[1]);
-  // TODO: Use AddToOpen to add the starting node to the open vector.
+  // Use AddToOpen to add the starting node to the open vector.
   AddToOpen(x, y, g, h, open, grid);
   
   cout << "No path found!" << "\n";
   return vector<vector<State>>{};
 }
 
+/**
+ * Format the string/output 
+ */
 std::string CellString(State cell) {
   switch(cell) {
     case State::kObstacle : return "⛰️   ";
@@ -80,8 +96,6 @@ std::string CellString(State cell) {
   }
 }
 
-
-// TODO: Add PrintBoard function here.
 void PrintBoard(const vector<vector<State>> board) {
   for (int i = 0; i < board.size(); i++) {
     for (int j = 0; j < board[i].size(); j++) {
@@ -92,24 +106,13 @@ void PrintBoard(const vector<vector<State>> board) {
     cout << "\n";
   }
 }
-/*
-void PrintBoard(const vector<vector<int>> &b) {
-  for (const vector<State> &v: b) {
-    for (const int &i: v) {
-      cout << CellString(i);
-    }
-    cout << "\n";
-  }
-}
-*/
 
 int main() {
-  // TODO: Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
+  // Declare "init" and "goal" arrays with values {0, 0} and {4, 5} respectively.
   int init[2]{0, 0};
   int goal[2]{4, 5};
   auto board = ReadBoardFile("1.board");
-  // TODO: Call Search with "board", "init", and "goal". Store the results in the variable "solution".
+  // Call Search with "board", "init", and "goal". Store the results in the variable "solution".
   vector<vector<State>> solution = Search(board, init, goal);
-  // TODO: Change the following line to pass "solution" to PrintBoard.
   PrintBoard(solution);
 }
